a57321ba292277433a942be16a49fb9a62363f1a
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-09-22 15:32:38 -0500

    fixed issues

--- src/hornedBeast.js
+++ src/hornedBeast.js
@@ -17,20 +17,17 @@ class Beast extends React.Component {
   render() {
     return (
       <>
-        <Card style={{ width: "18rem"}} className="h-100 mx-auto px-1"> //style to here
-        <Card.Img className='py-1 h-50 m-auto' variant="top" src={this.state.src} onClick={() => {
-                    this.props.setShowModalTrue(this.props.id);
-                }}/> //21 to here
+        <Card style={{ width: "18rem"}} className="h-100 mx-auto px-1"> 
+        <Card.Img className='py-1 h-50 m-auto' variant="top" src={this.props.src} onClick={() => {
+                    this.props.setShowModalTrue(this.props.id)
+                }}/> 
 
 
-          <Card.Img variant = "top" id="imgBeast" src={this.props.src} alt={this.props.alt} title={this.props.title} />
+          {/* <Card.Img variant = "top" id="imgBeast" src={this.props.src} alt={this.props.alt} title={this.props.title} /> */}
           <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
             <Card.Text>{this.props.description}</Card.Text>
             <Card.Text>{this.state.favorites}</Card.Text>
-
-            <Button className='mb-3' variant="secondary" onClick={this.handleClick}>Go somewhere</Button> //here
-
           </Card.Body>
           <Button variant="primary" onClick={this.increaseFavorites}>Like</Button>
         </Card>
